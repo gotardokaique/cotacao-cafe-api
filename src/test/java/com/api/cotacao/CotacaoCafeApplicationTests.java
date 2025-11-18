093578efd83a6e3357bf1b2bc3975763325197ba
kaiquecgotardo <kaiquecgotardo@gmail.com>
2025-11-17 20:57:33 -0400

    feat: commita gitigone e properties example

---
+++ src/test/java/com/api/cotacao/CotacaoCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacaoCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
